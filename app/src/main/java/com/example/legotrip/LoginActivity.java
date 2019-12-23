0ff3e966931ecb62ece8caf9278b6c89761f938b
dltpdus0516 <41175782+dltpdus0516@users.noreply.github.com>
2019-12-23 16:48:58 +0900

    .

--- app/src/main/java/com/example/legotrip/LoginActivity.java
+++ app/src/main/java/com/example/legotrip/LoginActivity.java
@@ -2,15 +2,37 @@ package com.example.legotrip;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button loginBtn, signupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
 
-    
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        signupBtn = (Button) findViewById(R.id.signupBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent login = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(login);
+            }
+        });
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signup = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(signup);
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/legotrip/SignupActivity.java
+++ app/src/main/java/com/example/legotrip/SignupActivity.java
@@ -1,4 +1,41 @@
 package com.example.legotrip;
 
-public class SignupActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class SignupActivity extends Activity {
+
+    Button signup_signupBtn;
+    ImageView back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        signup_signupBtn = (Button)findViewById(R.id.signup_signupBtn);
+        back = (ImageView)findViewById(R.id.back);
+
+        signup_signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signup = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(signup);
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
 }
