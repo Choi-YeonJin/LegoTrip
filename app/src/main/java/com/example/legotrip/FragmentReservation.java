e867002bd8de84c64a471eecb18b844df93fd13e
dltpdus0516 <41175782+dltpdus0516@users.noreply.github.com>
2019-12-26 08:55:59 +0900

    bottombar

---
+++ app/src/main/java/com/example/legotrip/FragmentReservation.java
@@ -0,0 +1,15 @@
+package com.example.legotrip;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentReservation extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_reservation, container, false);
+    }
+}
--- app/src/main/java/com/example/legotrip/HomeActivity.java
+++ app/src/main/java/com/example/legotrip/HomeActivity.java
@@ -1,30 +1,61 @@
 package com.example.legotrip;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
 public class HomeActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private FragmentMain fragmentMain= new FragmentMain();
+    private FragmentSetting fragmentSetting = new FragmentSetting();
+    private FragmentDiary fragmentDiary = new FragmentDiary();
+    private FragmentReservation fragmentReservation = new FragmentReservation();
+    private FragmentPlan fragmentPlan = new FragmentPlan();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.plan, R.id.reservation, R.id.main, R.id.diary, R.id.setting)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.framelayout, fragmentMain).commitAllowingStateLoss();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
     }
 
+    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+            switch(menuItem.getItemId())
+            {
+                case R.id.plan:
+                    transaction.replace(R.id.framelayout, fragmentPlan).commitAllowingStateLoss();
+                    break;
+                case R.id.reservation:
+                    transaction.replace(R.id.framelayout, fragmentReservation).commitAllowingStateLoss();
+                    break;
+                case R.id.main:
+                    transaction.replace(R.id.framelayout, fragmentMain).commitAllowingStateLoss();
+                    break;
+                case R.id.diary:
+                    transaction.replace(R.id.framelayout, fragmentDiary).commitAllowingStateLoss();
+                    break;
+                case R.id.setting:
+                    transaction.replace(R.id.framelayout, fragmentSetting).commitAllowingStateLoss();
+                    break;
+            }
+            return true;
+        }
+    }
 }
